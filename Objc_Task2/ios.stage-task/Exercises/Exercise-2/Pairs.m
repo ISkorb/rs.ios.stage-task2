0debe830bdc506aaf3bbf15ada9574c38d1359d2
ISkorb <ina.skorb@gmail.com>
2021-05-16 19:53:30 +0300

    feat: add task 2 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -2,7 +2,37 @@
 
 @implementation Pairs
 
-- (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
+- (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number
+{
+    @autoreleasepool
+    {
+        int size = (int)array.count;
+        int arrayOfInt[size];
+        
+        for(int i = 0; i < size; i++)
+        {
+            int currentValue = [(NSNumber *)[array objectAtIndex:i] intValue];
+            arrayOfInt[i] = currentValue;
+        }
+        
+        int differValue = [(NSNumber *)number intValue];
+        int count = 0;
+        
+        for(int j = size -1; j >= 0; j--)
+        {
+            for(int k = size - 1; k >= 0; k--)
+            {
+                int difference = arrayOfInt[j] - arrayOfInt[k];
+                
+                if(difference == differValue)
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return (NSInteger)count;
+    }
     return 0;
 }
 
