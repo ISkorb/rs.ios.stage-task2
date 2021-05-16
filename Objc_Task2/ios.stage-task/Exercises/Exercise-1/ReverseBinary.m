93917f7f53a1a9f51b34254c20366a85feece14c
ISkorb <ina.skorb@gmail.com>
2021-05-16 18:52:54 +0300

    feat: add task 1 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
-UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+UInt8 ReverseInteger(UInt8 n)
+{
+    unsigned int  numberOfBits = sizeof(n) * 8;
+    unsigned int reverseNum = 0, i, temp;
+      
+    for (i = 0; i < numberOfBits; i++)
+    {
+        temp = (n & (1 << i));
+        if(temp)
+        {
+            reverseNum |= (1 << ((numberOfBits - 1) - i));
+        }
+    }
+       
+    return reverseNum;
 }
