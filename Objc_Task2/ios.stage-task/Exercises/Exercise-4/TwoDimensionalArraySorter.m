32e128c5a32b732b56dbc98eec780f63f385e266
ISkorb <ina.skorb@gmail.com>
2021-05-17 00:39:06 +0300

    feat: add task 4 implementation

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -2,7 +2,62 @@
 
 @implementation TwoDimensionalArraySorter
 
-- (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
+- (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array
+{
+    @autoreleasepool
+    {
+        if(array == nil || ![array[0] isKindOfClass:[NSArray class]])
+        {
+            return @[];
+        }
+        
+        NSMutableArray* numbersArray = [[NSMutableArray alloc] init];
+        NSMutableArray* stringsArray = [[NSMutableArray alloc] init];
+        
+        for(NSArray* subArray in array)
+        {
+            if([subArray[0] isKindOfClass:[NSString class]])
+            {
+                for(NSString* str in subArray)
+                {
+                    [stringsArray addObject:str];
+                }
+            }
+            
+            if([subArray[0] isKindOfClass:[NSNumber class]])
+            {
+                for(NSNumber* num in subArray)
+                {
+                    [numbersArray addObject:num];
+                }
+            }
+        }
+        
+        NSSortDescriptor* lowesToHighest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
+        NSSortDescriptor* highestToLowest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO];
+        
+        if(stringsArray.count > 0 && numbersArray.count > 0)
+        {
+            [numbersArray sortUsingDescriptors:[NSArray arrayWithObject:lowesToHighest]];
+            [stringsArray sortUsingDescriptors:[NSArray arrayWithObject:highestToLowest]];
+            
+            NSMutableArray* mixedArray = [[NSMutableArray alloc] init];
+            [mixedArray addObject:numbersArray];
+            [mixedArray addObject:stringsArray];
+            
+            return mixedArray;
+        }
+        else if(stringsArray.count > 0)
+        {
+            [stringsArray sortUsingDescriptors:[NSArray arrayWithObject:lowesToHighest]];
+            return stringsArray;
+        }
+        else if(numbersArray.count > 0)
+        {
+            [numbersArray sortUsingDescriptors:[NSArray arrayWithObject:lowesToHighest]];
+            return numbersArray;
+        }
+    }
     return @[];
 }
 
